54e78e0a1b607f9b56ca431e12025e7b9e1361b7
Wertualistic <abduxalilovmuhammadali1@gmail.com>
2022-12-01 17:51:09 +0500

    this is responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Navbar.css';
 
 function Navbar() {
+    const [toggle, setToggle] = useState(true);
   return (
     <nav>
         <div className="nav_brand">
             <h1>Maqola</h1>
         </div>
-        <div className="search_bar">
+        {toggle ? 
+        (
+            <div className='all alll'>
+                <div className="search_bar">
             <input
                 type="search"
                 placeholder="Izlash..."
@@ -16,15 +20,24 @@ function Navbar() {
             /><i className='fas fa-search'></i>
         </div>
         <div className="menu">
-            <li><a href="">Bosh sahifa</a></li>
-            <li><a href="">Yozish</a></li>
-            <li><a href="">Kirish</a></li>
+            <li><a href="/#">Bosh sahifa</a></li>
+            <li><a href="/#">Yozish</a></li>
+            <li><a href="/#">Kirish</a></li>
         </div>
         <div className="auth">
             <button>Ro'yxatdan o'tish</button>
         </div>
-        <div className="notifications">
+            </div>
+        ):
+            (
+                ''
+            )
+        }
+         <div className="notifications">
             <i className='fa-solid fa-bell'><span>1</span></i>
+        </div>
+        <div className="burger_menu" onClick={() => setToggle(!toggle)}>
+            <i className='fas fa-bars'></i>
         </div>
     </nav>
   )
